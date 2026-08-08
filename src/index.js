cf77e5540239a50c2404ceb0a09f4c48c35dd276
Shannu <narayanasettishanmukh15@gmail.com>
2026-08-08 16:28:23 +0530

    Fix MongoDB connection

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 const express = require("express");
 require("dotenv").config();
 
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const connectDB = require("../config/database");
+
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({
